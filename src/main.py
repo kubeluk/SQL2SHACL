23ef197d33fdd2da654989b5b8f06da8ceb0fc0e
Lukas Kubelka <5943528-kubeluk@users.noreply.gitlab.com>
2024-06-18 11:54:24 +0200

    add: parse DDL function

--- src/main.py
+++ src/main.py
@@ -1,48 +1,94 @@
 import sqlparse
-from sqlparse.sql import Identifier, Parenthesis
-from sqlparse.tokens import Name, Keyword
-
-DDL = """
-    CREATE TABLE Emp (
-        E_id integer PRIMARY KEY,
-        Name text NOT NULL,
-        Post text
-    );
-    CREATE TABLE Acc (
-        A_id integer PRIMARY KEY,
-        Name text UNIQUE
-    );
-    CREATE TABLE Prj (
-        P_id integer PRIMARY KEY,
-        Name text NOT NULL,
-        ToAcc integer NOT NULL UNIQUE REFERENCES Acc (A_id)
-    );
-    CREATE TABLE Asg (
-        ToEmp integer REFERENCES Emp (E_id),
-        ToPrj integer REFERENCES Prj (P_id),
-        PRIMARY KEY (ToEmp, ToPrj)
-    );
-"""
+from sqlparse.sql import Identifier, Parenthesis, Token
+from sqlparse.tokens import Name, Punctuation
+from typing import List, Dict
+from pprint import pprint
 
 
-def main():
-    parsed = sqlparse.parse(DDL)
+def _is_punctuation_part_of_constraint(punct: Token, tkn_list: List[Token]) -> bool:
+    """Returns if punctionation is part of a constraint.
+
+    This happens when specifying key constraints like:
+        PRIMARY KEY (x_id, y_id)
+    """
+
+    punct_idx = tkn_list.index(punct)
+    tkn_before = tkn_list[punct_idx - 1]
+    tkn_after = tkn_list[punct_idx + 1]
+
+    # check if punctionation is surrounded by column names
+    if tkn_before.match(Name, None) and tkn_after.match(Name, None):
+        return True
+
+    return False
+
+
+def parse_ddl(ddl: str) -> Dict[str, List[List[Token]]]:
+    """TODO:"""
+
+    parsed = sqlparse.parse(ddl)
+    relation_details = {}
+
     for stmt in parsed:
-        if stmt.get_type() == "CREATE":
-            for tkn in stmt.tokens:
-                if isinstance(tkn, Identifier):
-                    print(f"rel: {tkn.get_real_name()}")
-                if isinstance(tkn, Parenthesis):
-                    for sub in tkn.flatten():
-                        if not sub.is_whitespace:
-                            # print(sub.ttype, sub)
-                            if sub.match(Name, None):
-                                print(f"col: {sub}")
-                            if sub.match(Name.Builtin, None):
-                                print(f"datatype: {sub}")
-                            if sub.match(Keyword, None):
-                                print(f"constraint: {sub}")
+        relation_name = None
+        expressions = []
+
+        for tkn in stmt.tokens:
+            if isinstance(tkn, Identifier):
+                relation_name = tkn.get_real_name()
+
+            if isinstance(tkn, Parenthesis):
+                parenthesis_content = list(tkn.flatten())[1:-1]
+                non_whitespace_subtkns = [
+                    sub for sub in parenthesis_content if not sub.is_whitespace
+                ]
+                expression_ = []
+
+                for idx, sub in enumerate(non_whitespace_subtkns):
+                    if idx == (len(non_whitespace_subtkns) - 1):
+                        # last subtkn in list
+                        expression_.append(sub)
+                        expressions.append(expression_)
+                        expression_ = []
+
+                    if sub.match(Punctuation, ","):
+                        if not _is_punctuation_part_of_constraint(
+                            sub, non_whitespace_subtkns
+                        ):
+                            # punctionation marks end of expression
+                            expressions.append(expression_)
+                            expression_ = []
+                            continue
+
+                    expression_.append(sub)
+
+        relation_details[relation_name] = expressions
+
+    return relation_details
 
 
 if __name__ == "__main__":
-    main()
+    DDL = """
+        CREATE TABLE Emp (
+            E_id integer PRIMARY KEY,
+            Name text NOT NULL,
+            Post text
+        );
+        CREATE TABLE Acc (
+            A_id integer PRIMARY KEY,
+            Name text UNIQUE
+        );
+        CREATE TABLE Prj (
+            P_id integer PRIMARY KEY,
+            Name text NOT NULL,
+            ToAcc integer NOT NULL UNIQUE REFERENCES Acc (A_id)
+        );
+        CREATE TABLE Asg (
+            ToEmp integer REFERENCES Emp (E_id),
+            ToPrj integer REFERENCES Prj (P_id),
+            PRIMARY KEY (ToEmp, ToPrj)
+        );
+    """
+
+    relation_details = parse_ddl(DDL)
+    pprint(relation_details)
