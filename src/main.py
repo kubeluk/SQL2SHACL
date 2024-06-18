f0d411879714ded81988bd1ee788c5d408e800fa
Lukas Kubelka <5943528-kubeluk@users.noreply.gitlab.com>
2024-06-18 15:39:39 +0200

    refactor: move some code to separate functions

--- src/main.py
+++ src/main.py
@@ -1,30 +1,54 @@
 import sqlparse
-from sqlparse.sql import Identifier, Parenthesis, Token
+from sqlparse.sql import Identifier, Parenthesis, Token, TokenList
 from sqlparse.tokens import Name, Punctuation
 from typing import List, Dict
 from pprint import pprint
 
 
-def _is_punctuation_part_of_constraint(punct: Token, tkn_list: List[Token]) -> bool:
-    """Returns if punctionation is part of a constraint.
+def _is_punctuation_end_of_expression(punct: Token, expression: List[Token]) -> bool:
+    """Returns if punctionation is the end of an expression.
 
-    This happens when specifying key constraints like:
-        PRIMARY KEY (x_id, y_id)
+    This is the case in every column definition but the last one, e.g.:
+        CREATE TABLE Emp (
+            E_id integer PRIMARY KEY,
+            Name text NOT NULL,
+            Post text
+        );
+
+    This is not the case when specifying key constraints, e.g.:
+        CREATE TABLE Asg (
+            ToEmp integer REFERENCES Emp (E_id),
+            ToPrj integer REFERENCES Prj (P_id),
+            PRIMARY KEY (ToEmp, ToPrj)
+        );
     """
 
-    punct_idx = tkn_list.index(punct)
-    tkn_before = tkn_list[punct_idx - 1]
-    tkn_after = tkn_list[punct_idx + 1]
+    punct_idx = expression.index(punct)
+    tkn_before = expression[punct_idx - 1]
+    tkn_after = expression[punct_idx + 1]
 
-    # check if punctionation is surrounded by column names
+    # check if punctionation is surrounded by column names (= marks key constraint)
     if tkn_before.match(Name, None) and tkn_after.match(Name, None):
-        return True
+        return False
 
-    return False
+    return True
+
+
+def _is_last_tkn_in_list(tkn: Token, tkn_list: List[Token]) -> bool:
+    """Returns True if the token is the last element in the list."""
+
+    return tkn_list.index(tkn) == (len(tkn_list) - 1)
+
+
+def _get_parenthesis_subtkns(parenthesis_tkn: TokenList) -> List[Token]:
+    """Returns the list of tokens with the starting and ending parentheses and 'Whitespace' removed."""
+
+    content = list(parenthesis_tkn.flatten())[1:-1]
+    return [tkn for tkn in content if not tkn.is_whitespace]
 
 
 def parse_ddl(ddl: str) -> Dict[str, List[List[Token]]]:
-    """TODO:"""
+    """Parses SQL DDL constructing tables into a the respective columns, data types and constraints."""
 
     parsed = sqlparse.parse(ddl)
     relation_details = {}
@@ -38,29 +62,22 @@ def parse_ddl(ddl: str) -> Dict[str, List[List[Token]]]:
                 relation_name = tkn.get_real_name()
 
             if isinstance(tkn, Parenthesis):
-                parenthesis_content = list(tkn.flatten())[1:-1]
-                non_whitespace_subtkns = [
-                    sub for sub in parenthesis_content if not sub.is_whitespace
-                ]
+                subtkns = _get_parenthesis_subtkns(tkn)
                 expression_ = []
 
-                for idx, sub in enumerate(non_whitespace_subtkns):
-                    if idx == (len(non_whitespace_subtkns) - 1):
-                        # last subtkn in list
-                        expression_.append(sub)
+                for subtkn_ in subtkns:
+                    if _is_last_tkn_in_list(subtkn_, subtkns):
+                        expression_.append(subtkn_)
                         expressions.append(expression_)
                         expression_ = []
 
-                    if sub.match(Punctuation, ","):
-                        if not _is_punctuation_part_of_constraint(
-                            sub, non_whitespace_subtkns
-                        ):
-                            # punctionation marks end of expression
+                    if subtkn_.match(Punctuation, ","):
+                        if _is_punctuation_end_of_expression(subtkn_, subtkns):
                             expressions.append(expression_)
                             expression_ = []
                             continue
 
-                    expression_.append(sub)
+                    expression_.append(subtkn_)
 
         relation_details[relation_name] = expressions
 
