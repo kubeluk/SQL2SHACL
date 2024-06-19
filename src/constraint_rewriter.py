d77c916cf36871e7ed399e7a6ef9e4f9f12f3ae7
Lukas Kubelka <5943528-kubeluk@users.noreply.gitlab.com>
2024-06-19 15:55:45 +0200

    add: template modules

---
+++ src/constraint_rewriter.py
@@ -0,0 +1,8 @@
+def apply_constraint_rewriting():
+    """Does the Constraint Rewriting from SQL to SHACL
+
+    See Thapa2021 [1] for more details.
+
+    [1] http://urn.nb.no/URN:NBN:no-90764
+    """
+    pass
