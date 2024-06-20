72b0a146f00a27a44f5d420fc850028a8c01110e
Lukas Kubelka <5943528-kubeluk@users.noreply.gitlab.com>
2024-06-20 20:27:38 +0200

    feat: finish column constraint handling

--- src/shacl_provider.py
+++ src/shacl_provider.py
@@ -1,127 +1,120 @@
-from typing import Union
 from rdflib import Graph, URIRef, BNode, Literal, Namespace
 from rdflib.namespace import RDF, RDFS, SH
 
 UQ = Namespace("http://sirius−labs.no/shapes/unique#")
 
 
-class Base:
+class Shape:
 
-    def __init__(self, path: URIRef):
+    def __init__(self):
         self.g = Graph()
+
+    def __iter__(self):
+        return iter(self.g)
+
+    def __iadd__(self, other):
+        if isinstance(other, Graph):
+            other += self.g
+        else:
+            raise TypeError("Cannot add Shape to non-Graph object")
+        return self
+
+    @property
+    def graph(self) -> Graph:
+        return self.g
+
+
+class Prop(Shape):
+
+    def __init__(self, rel: URIRef, path: URIRef, cls: URIRef):
+        super().__init__()
         self._b = BNode()
-        self.path = path
+        self.g.add((rel, SH.property, self._b))
         self.g.add((self._b, SH.path, path))
-
-
-class Prop(Base):
-
-    def __init__(self, path: URIRef, cls: URIRef):
-        super().__init__(path)
         self.g.add((self._b, SH.nodeKind, SH.IRI))
         self.g.add((self._b, SH["class"], cls))
 
 
 class MaxProp(Prop):
 
-    def __init__(self, path: URIRef, cls: URIRef):
-        super().__init__(path, cls)
+    def __init__(self, rel: URIRef, path: URIRef, cls: URIRef):
+        super().__init__(rel, path, cls)
         self.g.add((self._b, SH.maxCount, Literal(1)))
 
 
 class CrdProp(MaxProp):
 
-    def __init__(self, path: URIRef, cls: URIRef):
-        super().__init__(path, cls)
+    def __init__(self, rel: URIRef, path: URIRef, cls: URIRef):
+        super().__init__(rel, path, cls)
         self.g.add((self._b, SH.minCount, Literal(1)))
 
 
 class InvProp(Prop):
 
-    _b_inv = BNode()
-
-    def __init__(self, inv_path: URIRef, cls: URIRef):
-        super().__init__(self._b_inv, cls)
-        self.g.add((self._b_inv, SH.inversePath, inv_path))
+    def __init__(self, rel: URIRef, inv_path: URIRef, cls: URIRef):
+        _b = BNode()
+        super().__init__(rel, _b, cls)
+        self.g.add((_b, SH.inversePath, inv_path))
 
 
 class InvMaxProp(InvProp):
 
-    def __init__(self, inv_path: URIRef, cls: URIRef):
-        super().__init__(inv_path, cls)
+    def __init__(self, rel: URIRef, inv_path: URIRef, cls: URIRef):
+        super().__init__(rel, inv_path, cls)
         self.g.add((self._b, SH.maxCount, Literal(1)))
 
 
-class Data(Base):
+class Data(Shape):
 
-    def __init__(self, path: URIRef, dtype: URIRef):
-        super().__init__(path)
+    def __init__(self, rel: URIRef, path: URIRef, dtype: URIRef):
+        super().__init__()
+        self._b = BNode()
+        self.g.add((rel, SH.property, self._b))
+        self.g.add((self._b, SH.path, path))
         self.g.add((self._b, SH.nodeKind, SH.Literal))
         self.g.add((self._b, SH.datatype, dtype))
 
 
 class MaxData(Data):
 
-    def __init__(self, path: URIRef, dtype: URIRef):
-        super().__init__(path, dtype)
+    def __init__(self, rel: URIRef, path: URIRef, dtype: URIRef):
+        super().__init__(rel, path, dtype)
         self.g.add((self._b, SH.maxCount, Literal(1)))
 
 
 class CrdData(MaxData):
 
-    def __init__(self, path: URIRef, dtype: URIRef):
-        super().__init__(path, dtype)
+    def __init__(self, rel: URIRef, path: URIRef, dtype: URIRef):
+        super().__init__(rel, path, dtype)
         self.g.add((self._b, SH.minCount, Literal(1)))
 
 
-class UnqTuple:
+class UnqTuple(Shape):
 
-    def __init__(self, cls: URIRef, unq_prop: URIRef, _b_unq: BNode):
-        self.g = Graph()
-        self.g.add((_b_unq, UQ["unqProp"], unq_prop))
-        self.g.add((_b_unq, UQ["unqForClass"], cls))
+    def __init__(self, rel: URIRef, *unq_props: URIRef):
+        super().__init__()
+        _b = BNode()
+        self.g.add((rel, UQ["uniqueValuesForClass"], _b))
+        for unq_prop_ in unq_props:
+            self.g.add((_b, UQ["unqProp"], unq_prop_))
+        self.g.add((_b, UQ["unqForClass"], rel))
 
 
-class Shape:
+class Node(Shape):
 
-    def __init__(self, node: URIRef):
-        self.g = Graph()
-        self.unq_component = Graph()
-        # TODO: does every Shape need this unq_component or do I need to add it only once?
-        self._b_unq = BNode()
-        self.node = node
-        self.g.add((node, RDF.type, SH.NodeShape))
-        self.g.add((node, RDF.type, RDFS.Class))
-
-    def add(self, x: Union[Base, UnqTuple]) -> None:
-        if isinstance(x, Base):
-            self._add_property(x)
-        elif isinstance(x, UnqTuple):
-            self._add_unique(x)
-        else:
-            raise ValueError(
-                f"Input should be of type Union[Base, UnqTuple]. You gave {type(x)}."
-            )
-
-    def _add_property(self, p: Base) -> None:
-        self.g.add((self.node, SH.property, p._b))
-        self.g += p.g
-
-    def _add_unique(self, u: UnqTuple) -> None:
-        if len(self.unq_component) == 0:
-            self.unq_component = self.unq_component.parse(
-                "src/components/unique_values_constraint.ttl", format="ttl"
-            )
-            self.g += self.unq_component
-
-        self.g.add((self.node, UQ["uniqueValuesForClass"], self._b_unq))
-        self.g += u.g
+    def __init__(self, rel: URIRef):
+        super().__init__()
+        self.g.add((rel, RDF.type, SH.NodeShape))
+        self.g.add((rel, RDF.type, RDFS.Class))
 
 
 if __name__ == "__main__":
-    shape = Shape(URIRef("http://example.org/shapes#1"))
-    x = UnqTuple(
-        URIRef("http://example.org/props#1"), URIRef("http://example.org/props#2")
-    )
-    shape.add(x)
-    print(shape.g.serialize())
+    uri = URIRef("http://example.org/shapes#1")
+    n = Node(uri)
+    p = UnqTuple(uri, uri + "UNQ")
+
+    g = Graph()
+    g += n
+    g += p
+
+    print(g.serialize())
--- src/shacl_shaper.py
+++ src/shacl_shaper.py
@@ -1,8 +1,18 @@
 from typing import List, Dict
+from rdflib import Graph
 from sqlparse.sql import Token
-from sqlparse.tokens import Name, Punctuation, Keyword
+from sqlparse.tokens import Name, Keyword
 from table_parser import parse_ddl
-from shacl_provider import Shape, MaxData, CrdData, UnqTuple
+from shacl_provider import (
+    Node,
+    MaxData,
+    CrdData,
+    UnqTuple,
+    CrdProp,
+    MaxProp,
+    InvMaxProp,
+    InvProp,
+)
 from table_classifier import is_relation_binary
 from iri_builder import (
     build_class_iri,
@@ -13,114 +23,174 @@ from iri_builder import (
 )
 
 
-def _handle_table_constraint(shape: Shape, expression: List[Token]) -> None:
-    """Handles expressions that start with a Token of ttype Keyword.
+class Shaper:
 
-    This is the case for expressions that only define constraints, e.g.:
-        PRIMARY KEY (ToEmp, ToPrj)
-    """
+    def __init__(self, relation_details: Dict[str, List[Token]]):
+        self.shapes_graph = Graph()
+        self.relation_details = relation_details
+        self._unq_component_added = False
 
-    constraint = expression[0]
+    def _handle_table_constraint(self, expression: List[Token]) -> None:
+        """Handles expressions that start with a Token of ttype Keyword.
 
-    match constraint:
-        case "PRIMARY KEY":
-            pass
-        case "FOREIGN KEY":
-            pass
-        case "UNIQUE":
-            pass
-        case _:
-            # TODO: log unhandled constraint
-            return
+        This is the case for expressions that only define constraints, e.g.:
+            PRIMARY KEY (ToEmp, ToPrj)
+        """
 
+        constraint = expression[0]
 
-def _handle_datatype(
-    rel_name: str, col: Token, dtype: Token, shape: Shape, constraints: List[Token]
-) -> None:
-    """TODO"""
+        match constraint:
+            case "PRIMARY KEY":
+                pass
+            case "FOREIGN KEY":
+                pass
+            case "UNIQUE":
+                pass
+            case _:
+                # TODO: log unhandled constraint
+                return
 
-    attribute_uri = build_attribute_iri(rel_name, str(col))
-    mapped_xmlschema_type_uri = build_datatype_iri(str(dtype))
+    def _handle_datatype_col_constraint(
+        self, rel_name: str, col_name: str, dtype_name: str, constraints: List[Token]
+    ) -> None:
+        """TODO"""
 
-    for tkn in constraints:
-        if tkn.match(Keyword, "NOT NULL") or tkn.match(Keyword, "PRIMARY KEY"):
-            shape.add(CrdData(attribute_uri, mapped_xmlschema_type_uri))
-            return
+        rel_uri = build_class_iri(rel_name)
+        attribute_uri = build_attribute_iri(rel_name, col_name)
+        mapped_xmlschema_type_uri = build_datatype_iri(dtype_name)
 
-    shape.add(MaxData(attribute_uri, mapped_xmlschema_type_uri))
-    return
-
-
-def _handle_unique(
-    rel_name: str, col: Token, shape: Shape, constraints: List[Token]
-) -> None:
-    """TODO"""
-
-    for tkn in constraints:
-        if tkn.match(Keyword, "UNIQUE") or tkn.match(Keyword, "PRIMARY KEY"):
-            shape.add(
-                UnqTuple(
-                    shape.node, build_attribute_iri(rel_name, str(col)), shape._b_unq
+        for tkn in constraints:
+            if tkn.match(Keyword, "NOT NULL") or tkn.match(Keyword, "PRIMARY KEY"):
+                self.shapes_graph += CrdData(
+                    rel_uri, attribute_uri, mapped_xmlschema_type_uri
                 )
+                return
+
+        self.shapes_graph += MaxData(rel_uri, attribute_uri, mapped_xmlschema_type_uri)
+        return
+
+    def _ensure_unique_component(self) -> None:
+        if not self._unq_component_added:
+            unq_component = Graph().parse(
+                "src/components/unique_values_constraint.ttl", format="ttl"
             )
+            self.shapes_graph += unq_component
+            self._unq_component_added = True
 
+    def _handle_unique_col_constraint(
+        self, rel_name: str, col_name: str, constraints: List[Token]
+    ) -> None:
+        """TODO"""
 
-def _handle_primary_key(shape: Shape, expression: List[Token]) -> None:
-    for tkn in expression:
-        if str(tkn) == "PRIMARY KEY":
-            pass
+        for tkn in constraints:
+            if tkn.match(Keyword, "UNIQUE") or tkn.match(Keyword, "PRIMARY KEY"):
+                self.shapes_graph += UnqTuple(
+                    build_class_iri(rel_name), build_attribute_iri(rel_name, col_name)
+                )
 
+            self._ensure_unique_component()
 
-def _handle_references(shape: Shape, expression: List[Token]) -> None:
-    for tkn in expression:
-        if str(tkn) == "REFERENCES":
-            pass
+    def _handle_referenced_col(
+        self,
+        rel_name: str,
+        col_name: str,
+        referenced_rel_name: str,
+        referenced_col_name: str,
+        not_null: bool,
+        unique: bool,
+    ) -> None:
+        """TODO"""
 
+        rel_uri = build_class_iri(rel_name)
+        referenced_rel_uri = build_class_iri(referenced_rel_name)
+        path_obj_uri = build_foreign_key_iri(
+            rel_name, referenced_rel_name, [col_name], [referenced_col_name]
+        )
 
-def _handle_column_constraint(
-    rel_name: str, shape: Shape, expression: List[Token]
-) -> None:
-    """TODO"""
-
-    col = expression[0]
-    dtype = expression[1]
-    constraints = expression[2:]
-
-    _handle_datatype(rel_name, col, dtype, shape, constraints)
-
-    _handle_unique(rel_name, col, shape, constraints)
-
-
-def build_shapes(relation_details: dict) -> Dict[str, Shape]:
-    """Gets the output of table_parser.parse_ddl() and builds SHACL shapes from it.
-
-    Token.Name: column name
-    Token.Name.Builtin: data type
-    Token.Keyword: constraint
-    Token.Punctuation: parenthesis
-    """
-
-    shapes = {}
-
-    for rel_name, expressions in relation_details.items():
-        if not is_relation_binary(expressions):  # TODO: impelement is_relation_binary()
-            shape = Shape(build_class_iri(rel_name))
-            shapes[rel_name] = shape
-
-            # TODO: add logging which expressions haven't been handled
-            for expression_ in expressions:
-                first_tkn = expression_[0]
-
-                if first_tkn.match(Name, None):
-                    _handle_column_constraint(rel_name, shape, expression_)
-
-                if first_tkn.match(Keyword, None):
-                    _handle_table_constraint(shape, expression_)
-
+        if not_null:
+            self.shapes_graph += CrdProp(rel_uri, path_obj_uri, referenced_rel_uri)
         else:
-            pass  # TODO: handle binary relations
+            self.shapes_graph += MaxProp(rel_uri, path_obj_uri, referenced_rel_uri)
 
-    return shapes
+        if unique:
+            self.shapes_graph += InvMaxProp(referenced_rel_uri, path_obj_uri, rel_uri)
+        else:
+            self.shapes_graph += InvProp(referenced_rel_uri, path_obj_uri, rel_uri)
+
+    def _handle_references_col_constraint(
+        self, rel_name: str, col_name: str, constraints: List[Token]
+    ) -> None:
+        """TODO"""
+
+        not_null = False
+        unique = False
+
+        for idx, constraint_ in enumerate(constraints):
+            if constraint_.match(Keyword, "NOT NULL"):
+                not_null = True
+
+            if constraint_.match(Keyword, "UNIQUE"):
+                unique = True
+
+            if constraint_.match(Keyword, "REFERENCES"):
+                referenced_rel_name = str(constraints[idx + 1])
+                parenthesis_content = constraints[idx + 2 :]
+
+                for tkn in parenthesis_content:
+                    if tkn.match(Name, None):
+                        referenced_col_name = str(tkn)
+                        self._handle_referenced_col(
+                            rel_name,
+                            col_name,
+                            referenced_rel_name,
+                            referenced_col_name,
+                            not_null,
+                            unique,
+                        )
+
+    def _handle_column_constraint(self, rel_name: str, expression: List[Token]) -> None:
+        """TODO"""
+
+        col_name = str(expression[0])
+        dtype_name = str(expression[1])
+        constraints = expression[2:]
+
+        self._handle_datatype_col_constraint(
+            rel_name, col_name, dtype_name, constraints
+        )
+        self._handle_unique_col_constraint(rel_name, col_name, constraints)
+        self._handle_references_col_constraint(rel_name, col_name, constraints)
+
+    def build_shapes(self) -> Graph:
+        """Gets the output of table_parser.parse_ddl() and builds SHACL shapes from it.
+
+        Token.Name: column name
+        Token.Name.Builtin: data type
+        Token.Keyword: constraint
+        Token.Punctuation: parenthesis
+        """
+
+        for rel_name, expressions in self.relation_details.items():
+            if not is_relation_binary(
+                expressions
+            ):  # TODO: impelement is_relation_binary()
+                node_shape = Node(build_class_iri(rel_name))
+                self.shapes_graph += node_shape
+
+                # TODO: add logging which expressions haven't been handled
+                for expression_ in expressions:
+                    first_tkn = expression_[0]
+
+                    if first_tkn.match(Name, None):
+                        self._handle_column_constraint(rel_name, expression_)
+
+                    if first_tkn.match(Keyword, None):
+                        self._handle_table_constraint(expression_)
+
+            # else:
+            #     pass  # TODO: handle binary relations
+
+        return self.shapes_graph
 
 
 if __name__ == "__main__":
@@ -147,7 +217,7 @@ if __name__ == "__main__":
     """
 
     relation_details = parse_ddl(DDL)
-    shapes = build_shapes(relation_details)
+    shaper = Shaper(relation_details)
+    shapes_graph = shaper.build_shapes()
 
-    for shape_ in shapes.values():
-        print(shape_.g.serialize())
+    print(shapes_graph.serialize())
