e6ac9b1b01ea9d57eadd2cc3d0079e15238eb77f
Lukas Kubelka <5943528-kubeluk@users.noreply.gitlab.com>
2024-06-25 21:02:52 +0200

    feat: check existance of data type when parsing column

--- src/sql/column.py
+++ src/sql/column.py
@@ -1,25 +1,55 @@
 from typing import List, Tuple
 from sqlparse.sql import Token
-from sqlparse.tokens import Keyword
+from sqlparse.tokens import Keyword, Name
+from src.utils.exceptions import MissingSQLDatatypeException
 from .constraint import ColumnForeignKey
 
 
 class Column:
+    """
+    ```
+    <column definition> ::=
+        <column name> [ <data type or domain name> ]
+        [ <default clause> | <identity column specification> | <generation clause>
+        | <system time period start column specification>
+        | <system time period end column specification> ]
+        [ <column constraint definition>... ]
+        [ <collate clause> ]
+
+    <column constraint definition> ::=
+        [ <constraint name definition> ] <column constraint> [ <constraint characteristics> ]
+
+    <column constraint> ::=
+        NOT NULL
+        | <unique specification>
+        | <references specification>
+        | <check constraint definition>
+    ```
+    """
 
     def __init__(self, parent, col_name: str, expression: List[Token]):
         self._parent = parent
         self._name = col_name
         self._expression = expression
-        self._dtype = str(expression[0])
-        self._constraints = expression[1:]
-        self._unique, self._not_null, self._reference = self._set_column_properties()
+        self._dtype, self._unique, self._not_null, self._reference = (
+            self._set_column_properties()
+        )
 
-    def _set_column_properties(self) -> Tuple[bool, bool, ColumnForeignKey]:
+    def _set_column_properties(self) -> Tuple[str, bool, bool, ColumnForeignKey]:
+        if self._expression[0].match(Name.Builtin, None):
+            dtype = str(self._expression[0])
+
+        else:
+            raise MissingSQLDatatypeException(
+                f"Column <{self._name}> of relation <{self._parent.name}>"
+            )
+
         unique = False
         not_null = False
         reference = None
 
-        for idx, constraint_ in enumerate(self._constraints):
+        constraints = self._expression[1:]
+        for idx, constraint_ in enumerate(constraints):
             if constraint_.match(Keyword, "UNIQUE"):
                 unique = True
 
@@ -31,14 +61,14 @@ class Column:
                 not_null = True
 
             if constraint_.match(Keyword, "REFERENCES"):
-                constraint_args = self._constraints[idx + 1 :]
+                constraint_args = constraints[idx + 1 :]
 
                 reference = ColumnForeignKey(
                     self,
                     constraint_args,
                 )
 
-        return unique, not_null, reference
+        return dtype, unique, not_null, reference
 
     @property
     def name(self) -> str:
