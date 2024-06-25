07c1de022ed1c3b556f0d00d2f64b24a9e60279c
Lukas Kubelka <5943528-kubeluk@users.noreply.gitlab.com>
2024-06-25 21:03:43 +0200

    fix: add missing exception

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -1,3 +1,9 @@
+class MissingSQLDatatypeException(Exception):
+    """Raised when column definition does not contain a data type."""
+
+    pass
+
+
 class UnsupportedSQLDatatypeException(Exception):
     """Raised when SQL datatype is not found in XSDSchema mapping file."""
 
