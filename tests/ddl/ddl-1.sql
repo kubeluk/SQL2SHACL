0e3c85d5d19b2dc4033f232d157f08e463b5525f
Lukas Kubelka <5943528-kubeluk@users.noreply.gitlab.com>
2024-06-26 14:52:48 +0200

    chore: add test cases

---
+++ tests/ddl/ddl-1.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Emp (
+    E_id integer PRIMARY KEY,
+    Name char CONSTRAINT test_constraint NOT NULL,
+    Post char
+);
+CREATE TABLE Acc (
+    A_id integer,
+    Name char UNIQUE,
+    PRIMARY KEY (A_id)
+);
+CREATE TABLE Prj (
+    P_id integer PRIMARY KEY,
+    Name char NOT NULL,
+    ToAcc integer NOT NULL UNIQUE REFERENCES Acc (A_id)
+);
+CREATE TABLE Asg (
+    ToEmp integer REFERENCES Emp (E_id),
+    ToPrj integer REFERENCES Prj (P_id),
+    PRIMARY KEY (ToEmp, ToPrj)
+);
