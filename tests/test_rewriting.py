34f491511cc145b593fa66dfead3960435fc03f3
Lukas Kubelka <5943528-kubeluk@users.noreply.gitlab.com>
2024-06-28 17:18:34 +0200

    test: add example test case

--- tests/test_rewriting.py
+++ tests/test_rewriting.py
@@ -1,22 +1,19 @@
+import logging
+import sql2shacl
 from rdflib import Graph
-from rdflib.compare import isomorphic, similar
-from constraint_rewriter import ConstraintRewriter
-
-
-UNIQUE_COMPONENT_GRAPH = Graph().parse(
-    "src/components/unique_values_constraint.ttl", format="ttl"
-)
+from rdflib.compare import isomorphic
 
 
 def test_1():
     with open("tests/ddl/ddl-1.sql") as file:
         test_ddl = file.read()
 
-    cr = ConstraintRewriter.setup(test_ddl)
-    cr.rewrite()
-
-    generated_shapes_graph = cr.shapes_graph
+    shapes_graph = Graph().parse(
+        data=sql2shacl.rewrite(
+            test_ddl, out_path="tests/tmp/shapes_graph.ttl", log_level=logging.INFO
+        ),
+        format="ttl",
+    )
     actual_shapes_graph = Graph().parse("tests/shacl/shape-1.ttl")
-    actual_shapes_graph += UNIQUE_COMPONENT_GRAPH
 
-    assert similar(generated_shapes_graph, actual_shapes_graph) is True
+    assert isomorphic(shapes_graph, actual_shapes_graph) is True
